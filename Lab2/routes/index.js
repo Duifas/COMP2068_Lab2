54c76e68bd62acfa24c705d165f67eca7e35c3d2
Thales Valente <46799430+Duifas@users.noreply.github.com>
2020-06-22 11:00:22 -0400

    Sorry about that. Final changes

--- Lab2/routes/index.js
+++ Lab2/routes/index.js
@@ -4,7 +4,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+
+        var varX = req.query.x;
+    var varY = req.query.y;
+    var finalResult = 0;
+
+    console.log(req.query.method);
+        switch (req.query.method) {
+            case "add":
+                finalResult = parseFloat(varY) + parseFloat(varX);
+                res.render('index', { title: 'Addition', x: varX, y: varY, method: '+', result: finalResult });
+                break;
+            case "subtract":
+                finalResult = parseFloat(varX) - parseFloat(varY);
+                res.render('index', { title: 'Subtraction', x: varX, y: varY, method: '-', result: finalResult });
+                break;
+            case "multiply":
+                finalResult = parseFloat(varX) * parseFloat(varY);
+                res.render('index', { title: 'Multiplication', x: varX, y: varY, method: '*', result: finalResult });
+                break;
+            case "divide":
+                finalResult = parseFloat(varX) / parseFloat(varY);
+                res.render('index', { title: 'Division', x: varX, y: varY, method: '/', result: finalResult});
+                break;
+            default:
+                res.render('index', { title: 'Error', x: 0, y: 0, method : 'error' });
+                break;
+        }
+
 });
 
 module.exports = router;
